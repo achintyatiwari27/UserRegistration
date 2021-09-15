33d13ee3d939387c093011c491446f1f8e4bb05c
Achintya <achintyatiwari27>
2021-09-15 05:30:15 +0530

    [Added]:Code to add First name in user registration

---
+++ UC1_First_name.java
@@ -0,0 +1,26 @@
+package com.BridgeLabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Program!");
+        UserInput user = new UserInput();
+        user.fName("Achintya");
+    }
+
+    public static class UserInput {
+        public void fName(String fName) {
+            String regex = "^[A-Z]{1}[a-z]{2,}$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(fName);
+            boolean valid = matcher.matches();
+            if (valid == true) {
+                System.out.println(fName + " is Valid");
+            } else {
+                System.out.println(fName + " is Invalid");
+            }
+        }
+    }
+}
